83838feb546a6d949c3b9831c53a7a53389d5244
Sanghyuk Park <sanghyuk.park85@gmail.com>
2024-08-01 10:41:05 -0500

    use ConfigDict for orm mode

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, ConfigDict
 from datetime import datetime
 from typing import Optional
 
@@ -14,23 +14,21 @@ class PostCreate(PostBase):
 
 
 class UserOut(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    
     id: int
     email: EmailStr
     created_at: datetime
-    
-    class Config:
-        from_attributes = True
         
 
 class Post(PostBase):
+    model_config = ConfigDict(from_attributes=True)
+    
     id: int
     created_at: datetime
     owner_id: int
     owner: UserOut
     
-    class Config:
-        from_attributes = True
-
         
 class UserCreate(BaseModel):
     email: EmailStr
