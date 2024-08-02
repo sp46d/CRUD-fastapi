a6790c95bc4ea715d3cd38969b2653e836b0f24e
Sanghyuk Park <sanghyuk.park85@gmail.com>
2024-08-02 14:50:34 -0500

    update gitignore and implement direction of vote

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -9,28 +9,34 @@ router = APIRouter(
 )
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def vote(body: schemas.Vote,
+def vote(vote: schemas.Vote,
          db: Session = Depends(get_db),
          current_user: schemas.UserOut = Depends(oauth2.get_current_user)):
     
-    post_voted = db.query(models.Post).filter(models.Post.id == body.post_id).first()
+    vote_query = db.query(models.Vote).filter(models.Vote.post_id == vote.post_id,
+                                              models.Vote.user_id == current_user.id)
+    vote_found = vote_query.first()
+    
+    post_voted = db.query(models.Post).filter(models.Post.id == vote.post_id).first()
     if post_voted == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Post with id: {body.post_id} was not found")
+                            detail=f"post {vote.post_id} was not found")
     
-    vote_exist = db.query(models.Vote)\
-            .filter(models.Vote.post_id == body.post_id, models.Vote.user_id == current_user.id)\
-        
-    if vote_exist.first():
-        vote_exist.delete(synchronize_session=False)
-        post_voted.likes -= 1
-
-    else:
-        new_vote = models.Vote(post_id=body.post_id, user_id=current_user.id)
+    if vote.dir == 1:
+        if vote_found:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                                detail=f"user {current_user.id} already voted on post {vote.post_id}")
+        new_vote = models.Vote(post_id=vote.post_id, user_id=current_user.id)
         db.add(new_vote)
         post_voted.likes += 1
-        
+    else:
+        if vote_found:
+            vote_query.delete(synchronize_session=False)
+            post_voted.likes -= 1
+        else:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                                detail=f"user {current_user.id} did not vote on post {vote.post_id}, so cannot unvote post {vote.post_id}")
+            
     db.commit()
-    # db.refresh(new_vote)
-        
+    
     return post_voted
--- play.py
+++
@@ -1,5 +0,0 @@
-from app.utils import hash
-
-print(hash("Qkrtkdgur1!"))
-print(hash("Qkrtkdgur1!"))
-print(hash("Qkrtkdgur1!"))
