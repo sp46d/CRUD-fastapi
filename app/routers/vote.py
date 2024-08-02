e98945a1684ab813fa8d8290905cb4740137bfd0
Sanghyuk Park <sanghyuk.park85@gmail.com>
2024-08-02 14:58:42 -0500

    separate Post likes logic from vote function

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -8,35 +8,35 @@ router = APIRouter(
     tags=["Vote"]
 )
 
-@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+@router.post("/", status_code=status.HTTP_201_CREATED)
 def vote(vote: schemas.Vote,
          db: Session = Depends(get_db),
          current_user: schemas.UserOut = Depends(oauth2.get_current_user)):
     
-    vote_query = db.query(models.Vote).filter(models.Vote.post_id == vote.post_id,
-                                              models.Vote.user_id == current_user.id)
-    vote_found = vote_query.first()
-    
-    post_voted = db.query(models.Post).filter(models.Post.id == vote.post_id).first()
-    if post_voted == None:
+    post = db.query(models.Post).filter(models.Post.id == vote.post_id).first()
+    if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post {vote.post_id} was not found")
     
+    vote_query = db.query(models.Vote).filter(models.Vote.post_id == vote.post_id,
+                                              models.Vote.user_id == current_user.id)
+    found_vote = vote_query.first()
+    
     if vote.dir == 1:
-        if vote_found:
+        if found_vote:
             raise HTTPException(status_code=status.HTTP_409_CONFLICT,
                                 detail=f"user {current_user.id} already voted on post {vote.post_id}")
         new_vote = models.Vote(post_id=vote.post_id, user_id=current_user.id)
         db.add(new_vote)
-        post_voted.likes += 1
+        db.commit()
+
+        return {"message": "vote is successfully added"}
+        
     else:
-        if vote_found:
-            vote_query.delete(synchronize_session=False)
-            post_voted.likes -= 1
-        else:
+        if not found_vote:
             raise HTTPException(status_code=status.HTTP_409_CONFLICT,
                                 detail=f"user {current_user.id} did not vote on post {vote.post_id}, so cannot unvote post {vote.post_id}")
-            
-    db.commit()
-    
-    return post_voted
+        vote_query.delete(synchronize_session=False)
+        db.commit()
+        
+        return {"message": "vote is successfully deleted"}
