a8b721aa354dbad5ab461963b6547ac2b4cdcee4
Sanghyuk Park <sanghyuk.park85@gmail.com>
2024-08-22 12:58:35 -0500

    remove lazy='joined' because it conflicts with explicit join clause in query

--- app/models.py
+++ app/models.py
@@ -27,7 +27,7 @@ class Post(Base):
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
     owner_id: Mapped[int] = mapped_column(ForeignKey('users.id', ondelete='CASCADE'))
     
-    owner: Mapped['User'] = relationship(lazy='joined', back_populates='posts')
+    owner: Mapped['User'] = relationship(back_populates='posts')
     votes: Mapped[List['Vote']] = relationship(back_populates='post')
 
     
@@ -36,7 +36,7 @@ class Vote(Base):
     
     post_id: Mapped[int] = mapped_column(ForeignKey('posts.id', ondelete='CASCADE'), primary_key=True)
     user_id: Mapped[int] = mapped_column(ForeignKey('users.id', ondelete='CASCADE'), primary_key=True)
-    # created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
     
-    post: Mapped['Post'] = relationship(back_populates='votes')
-    user: Mapped['User'] = relationship(back_populates='votes')
+    post: Mapped['Post'] = relationship(lazy='joined', back_populates='votes')
+    user: Mapped['User'] = relationship(lazy='joined', back_populates='votes')
